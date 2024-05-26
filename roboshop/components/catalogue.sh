6fd72db4fe8288d0f42f84872e1b1c871ef2a017
KeerthiChawla <you@example.com>
2024-05-26 23:11:14 +0530

    Making changes to Catalogue script

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -30,7 +30,7 @@ Status_Check $?
 chown roboshop:roboshop -R /home/roboshop
 
 Print "Update SystemD Service"
-sed -i -e 's//MONGO_DNSNAME/mondodb.roboshop-internal/' /home/roboshop/catalogue/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mondodb.roboshop-internal/' /home/roboshop/catalogue/systemd.service
 Status_Check $?
 
 Print "SetUp SystemD Service"
