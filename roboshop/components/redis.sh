636f9615591369015b489c735f7284b518dab534
KeerthiChawla <you@example.com>
2024-05-26 23:26:53 +0530

    Making changes to Catalogue script

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "Install Yum Utilities & Download Redis Repos"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG 
+Status_Check $?
+
+Print "SetUp Redis Repos"
+yum-config-manager --enable remi &>>$LOG
+Status_Check $?
+
+Print "Install Redis"
+yum install redis -y &>>$LOG
+Status_Check $?
+
+Print "Configure Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf 
+Status_Check $?
+
+Print "Start Redis Database"
+
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+Status_Check $?
