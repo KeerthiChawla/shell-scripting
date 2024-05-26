2a603b29d35ac52ac03fe8d8450ca352e2ce8324
KeerthiChawla <you@example.com>
2024-05-26 22:17:16 +0530

    Making changes to Catalogue script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -11,7 +11,7 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
 Status_Check $?
 
 Print "Installing MongoDB\t"
-yum install -y mongodb-org &>>/tmp/log
+yum install -y mongodb-org &>>$LOG
 Status_Check $? 
 
 Print "Configuring MongoDB\t"
@@ -29,12 +29,12 @@ Status_Check $?
 
 cd /tmp
 Print "Extracting Schema Archive"
-unzip -o mongodb.zip &>>/tmp/log
+unzip -o mongodb.zip &>>$LOG
 Status_Check $? 
 cd mongodb-main
 Print "Loading Schema\t\t"
-mongo < catalogue.js &>>/tmp/log
-mongo < users.js &>>/tmp/log 
+mongo < catalogue.js &>>$LOG
+mongo < users.js &>>$LOG
 Status_Check $? 
 
 exit 0
