5c589776a59d136b15e10b163b7e4c6d5e5c30d9
KeerthiChawla <you@example.com>
2024-06-03 18:38:21 +0530

    pushing changes

--- create-instance.sh
+++ create-instance.sh
@@ -3,4 +3,4 @@
 LID="lt-0d2c299c1cfb51b34"
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq .Instances.InstanceId
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq .Instances[].InstanceId
