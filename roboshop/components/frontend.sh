3a48aaa3e9d7f45e7386c3ed851290296c1ab626
KeerthiChawla <you@example.com>
2024-06-14 12:30:26 +0530

    creating instances

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,26 +2,26 @@
 
 source components/common.sh
 
-Print "Install Nginx"
+Print "Install Nginx\t\t"
 yum install nginx -y &>>$LOG
 Status_Check $?
 
 Print "Download Frontend Archive"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 Status_Check $?
 
 Print "Extract Frontend Archive"
-rm -rf /usr/share/nginx/* &>>$LOG && cd /usr/share/nginx && unzip -o /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static html &>>$LOG
+rm -rf /usr/share/nginx/* && cd /usr/share/nginx && unzip -o /tmp/frontend.zip  &>>$LOG  && mv frontend-main/* .  &>>$LOG  &&   mv static html  &>>$LOG
 Status_Check $?
 
-Print "Copy Nginx Roboshop Config"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Print "Copy Nginx RoboShop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
 Status_Check $?
 
-Print "Update Nginx Roboshop Config"
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG
+Print "Update Nginx RoboShop Config"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf  &>>$LOG
 Status_Check $?
 
-Print "Restart Nginx"
-systemctl restart nginx &>>$LOG && systemctl enable nginx  &>>$LOG
+Print "Restart Nginx\t\t"
+systemctl restart nginx  &>>$LOG  && systemctl enable nginx   &>>$LOG
 Status_Check $?
